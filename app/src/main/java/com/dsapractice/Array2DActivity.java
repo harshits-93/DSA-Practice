5095f82c593f6bade536850f977b2a339a796f5c
harshits-93 <harshits4444@gmail.com>
2024-05-23 15:28:51 +0530

    New problems solved in 2d matrix.

--- app/src/main/java/com/dsapractice/Array2DActivity.java
+++ app/src/main/java/com/dsapractice/Array2DActivity.java
@@ -29,13 +29,156 @@ public class Array2DActivity extends AppCompatActivity {
 
 
         int matrix[][] = {{6, 6, 2, 28, 2},
-                          {12, 26, 3 ,28 ,7},
-                          {22, 25, 3 ,4 ,23},
+                {12, 26, 3, 28, 7},
+                {22, 25, 3, 4, 23},
         };
         ArrayList<Integer> listOfElementsTraversed = spirallyTraverse(matrix, 3, 5);
         Log.d(TAG, "Spiral traversal in matrix: " + listOfElementsTraversed);
-        /*exitElement();
+
+
+        int A[][] = {{1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}};
+
+        int k = findK(A, 4, 4, 10);
+        Log.d(TAG, "element present at kth position is : " + k);
+
+
+        int arr[][] = {{0, 1, 0}, {0, 1, 1}, {0, 0, 0}};
+        int[] result = FindExitPoint(9, 23, arr);
+        Log.d(TAG, "FindExitPoint: " + Arrays.toString(result));
+
+
+        int[][] matrix1 = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+
+        int n = 3;
+       // rotateBy90ClkWise(matrix1, n);
+        rotateBy90AntiClkWise(matrix1, n);
+        printMatrix(n, matrix);
+
+        /*
         searchIn2DMatrix(40);*/
+    }
+
+    private static void printMatrix(int n, int[][] matrix) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                System.out.print(matrix[i][j] + " ");
+        }
+    }
+
+    private void rotateBy90ClkWise(int matrix[][], int n) {
+        // Since it is n x n matrix
+        // n = matrix.length,
+        // n = matrix[0].length
+
+        /**
+         input                     90 degree rotate clkwise
+         1 2 3                        7 4 1
+         4 5 6        =>              8 5 2
+         7 8 9                        9 6 3
+
+         ||
+         Transpose
+         1 4 7
+         2 5 8
+         3 6 9
+
+         //solution is first we take transpose of input, after transpose we just need to reverse
+         //each row to obtain our desired result
+
+         */
+        // Taking transpose of matrix, here row are converted to columns, simply reverse [i][j] with
+        // [j][i]. Important, but we will not cover all elements, either upper diagonal or lower
+        // diagonal, because if traverse all element same elements will be reverse 2 times and the
+        //end result will be the same matrix not the reversed one.
+
+        //for row
+        //Here here got our transpose
+        for (int i = 0; i < matrix.length; i++) {
+            //for column, note, j is not initialized with 0 but with i itself, to prevent same
+            //elements to be swapped 2 times.
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        //reverse each row
+        for (int i = 0; i < matrix.length; i++) {
+            int left = 0;
+            int right = matrix[i].length - 1;
+
+            while (left < right) {
+
+                //swapping
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+
+    }
+    private void rotateBy90AntiClkWise(int matrix[][], int n) {
+        // Since it is n x n matrix
+        // n = matrix.length,
+        // n = matrix[0].length
+
+        /**
+         input                     90 degree rotate anti-clkwise
+         1 2 3                        3 6 9
+         4 5 6         =>             2 5 8
+         7 8 9                        1 4 7
+
+         ||
+         Transpose
+         1 4 7
+         2 5 8
+         3 6 9
+
+         //solution is first we take transpose of input, after transpose we just need to reverse
+         //each column to obtain our desired result
+
+         */
+
+        //for row
+        //Here we got our transpose
+        for (int i = 0; i < matrix.length; i++) {
+            //for column, note, j is not initialized with 0 but with i itself, to prevent same
+            //elements to be swapped 2 times.
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        //reverse each column
+        for (int j = 0; j < matrix[0].length; j++) {
+            int left = 0;
+            int right = matrix.length - 1;
+
+            while (left < right) {
+
+                //swapping
+                int temp = matrix[left][j];
+                matrix[left][j] = matrix[right][j];
+                matrix[right][j] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+
     }
 
     private void searchIn2DMatrix(int NumToFind) {
@@ -65,52 +208,80 @@ public class Array2DActivity extends AppCompatActivity {
 
     }
 
-    private void exitElement() {
-        int arr[][] = {{0, 1, 1, 0}, {0, 0, 0, 1}, {1, 0, 1, 0}, {1, 1, 0, 0}};
+    public int[] FindExitPoint(int n, int m, int[][] matrix) {
         //https://www.youtube.com/watch?v=FUBlb360kqU&list=PL-Jc9J83PIiFkOETg2Ybq-FMuJjkZSGeH&index=9
         // Here,In a matrix containing 0 and 1. we need to travel in same direction when 0
         // is found and if 1 is encountered we need to take a 90 degree turn, and finally
         // we need to return i,j i.e element location where the loop is exited. We will
         // start our travel from east direction
         //Here 0 means east direction, 90 degree from here goes to south, so for south we have 1,
-        // after that west is 2, north 3
+        // after that west is represented as 2, north as 3
+        //Initially we have our direction as east (i.e.dir = 0), if we getting 0 we want to remain
+        //in east direction only, if 1 encountered then 90 turn, so what we do is we add this 1 value
+        //to dir, dir = 0 +1 => dir = 1, 1 means direction is south now which is correct, similarly
+        //for west => dir = 1+1 => 2 , then for north =>  dir = 2+1 =>3 which represent north direction.
+        //Now, the thing if we encounter 1 then dir = 3+1 => 4 which is incorrect, we don't have fifth
+        //direction, on this we want dir to set as 0 again, hence we take modulas for complete
+        //statement (dir + arr[i][j]) % 4 i.e.  0 %4 = 0, 1%4 = 1, 2%4 = 2, 3%4 = 3 and our main
+        //edge case 4%4 =0 which is correct now.
+
+        //n = no of row = matrix.length
+        //m = no. of column = matrix[0].length
+
+        // code here
+        int[] result = new int[2];
+
         int dir = 0;
         int i = 0, j = 0;
 
         while (true) {
-            dir = (dir + arr[i][j]) % 4; // Main condition which to maintain direction
+            dir = (dir + matrix[i][j]) % 4; // Main condition which maintains direction
 
             if (dir == 0) { // east
+                //here we are resetting 1 to zero as per question requirement
+                matrix[i][j] = 0;
+                //only column increases, row remains same
                 j++;
             } else if (dir == 1) { // south
+                //only row increases, column remains same
+                matrix[i][j] = 0;
                 i++;
             } else if (dir == 2) { // west
+                //only column decreases, row remains same
+                matrix[i][j] = 0;
                 j--;
             } else if (dir == 3) { // north
+                //only row decreases, column remains same
+                matrix[i][j] = 0;
                 i--;
             }
-
+            //These are exit points, i<0 means it exit from top, i= -1, but before break we incremented
+            // i with one, because in final result we can't write (-1, column index), we should
+            // return previous value before exit.
             if (i < 0) {
                 i++;
                 break;
             }
+            // j<0 means it exit from left, i.e. j became j= -1
             if (j < 0) {
                 j++;
                 break;
             }
-            if (i == arr.length) {
+            // i == matrix.length means it exceed max row (max = matrix.length -1 ), exited from bottom
+            if (i == n) {
                 i--;
                 break;
             }
-            if (j == arr[0].length) {
+            //j == matrix[0].length means it exceed max column (max = matrix[0].length -1 ), exited from right
+            if (j == m) {
                 j--;
                 break;
             }
         }
+        result[0] = i;
+        result[1] = j;
 
-        Log.d(TAG, "exitElement: " + i + " " + j);
-
-
+        return result;
     }
 
 
@@ -156,6 +327,65 @@ public class Array2DActivity extends AppCompatActivity {
 
         return resultList;
 
+    }
+
+
+    /*Given a matrix with n rows and m columns. Your task is to find the kth element which is
+     obtained while traversing the matrix spirally. You need to complete the method findK which
+      takes four arguments the first argument is the matrix A and the next two arguments will be n
+      and m denoting the size of the matrix A and then the forth argument is an integer k. The
+      function will return the kth element obtained while traversing the matrix spirally.*/
+    int findK(int A[][], int n, int m, int k) {
+        // Your code here
+        int totalElements = n * m;
+        int count = 0;
+        int result = -1;
+        int minRow = 0, minCol = 0, maxRow = n - 1, maxCol = m - 1;
+
+        if (k > totalElements) {
+            return result;
+        }
+
+        while (count < totalElements) {
+
+            //Top Wall
+            for (int i = minRow, j = minCol; j <= maxCol && count < totalElements; j++) {
+                count++;
+                if (k == count) {
+                    return A[i][j];
+                }
+            }
+            minRow++;
+
+            //Right Wall
+            for (int i = minRow, j = maxCol; i <= maxRow && count < totalElements; i++) {
+                count++;
+                if (k == count) {
+                    return A[i][j];
+                }
+            }
+            maxCol--;
+
+            //Bottom Wall
+            for (int i = maxRow, j = maxCol; j >= minCol && count < totalElements; j--) {
+                count++;
+                if (k == count) {
+                    return A[i][j];
+                }
+            }
+            maxRow--;
+
+            //Left Wall
+            for (int i = maxRow, j = minCol; i >= minRow && count < totalElements; i--) {
+                count++;
+                if (k == count) {
+                    return A[i][j];
+                }
+            }
+            minCol++;
+        }
+
+        return result;
     }
 
     private void waveTraversal() {
